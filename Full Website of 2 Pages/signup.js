d7c51f58486a8041657e27ece860ff00650827c5
Rishav <78720189+rishav015@users.noreply.github.com>
2022-07-24 16:08:26 +0530

    Update signup.js

--- Full Website of 2 Pages/signup.js
+++ Full Website of 2 Pages/signup.js
@@ -1,31 +1,11 @@
-// var modal = document.getElementById('id01');
-
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         var scroll = $(window).scrollTop();
-//         if (scroll > 300) {
-//           $(".black").css("background" , "blue");
-//         }
-  
-//         else{
-//             $(".black").css("background" , "#333");  	
-//         }
-//     })
-//   });
 const inp = document.getElementById('email');
 const AInput = document.getElementById("password");
+
 inp.addEventListener('click', function onClick(event) {
     event.preventDefault();
   const input = document.getElementById('email');
 
-  
+
     input.style.backgroundColor = "rgb(252, 229, 216)";
     input.style.color = "black"
 
@@ -34,50 +14,35 @@ inp.addEventListener('click', function onClick(event) {
 AInput.addEventListener("click",function onClick(event){
     event.preventDefault();
   const AInput = document.getElementById("password");
-   
     AInput.style.backgroundColor = 'rgb(252, 229, 216)';
     AInput.style.color = "black"
-
 
 });
 
 
 
 
+
 let form = document.querySelector("form");
 
+
 let signupArr  = JSON.parse(localStorage.getItem("btn")) || [];
-// let loginArr  = JSON.parse(localStorage.getItem("btn")) ||[];
 
+form.addEventListener("submit",function(event){
+  event.preventDefault();
+  let obj = {
+    email:form.email.value,
+    password:form.password.value,
+  }
+  if(obj.email =="" && obj.password==""){
+    alert("Please Enter Your Email and Password");
+    return
+  }
+  else{
+    signupArr.push(obj);
+    localStorage.setItem("btn",JSON.stringify(signupArr));
+      window.location.href ="LoginPage.html"
+  }
 
-
-
-
-
-
-  document.querySelector("form").addEventListener("submit",function(event){
-    event.preventDefault();
-
-    if(signupArr.length===0){
-      alert("No user exist yet");
-      return;
-    }
-    let obj ={
-      email:form.email.value,
-      password:form.password.value,
-    }
-    signupArr.forEach(function(ele){
-      
-      if(ele.email===obj.email && ele.password == obj.password){
-        localStorage.setItem("btn1",JSON.stringify(ele));
-        alert("You have Successfully Logged in");
-        window.location.href="https://toggl.com/track/";
-      }
-      else{
-        window.alert("invalid email or password");
-      }
-    })
     
-    
-  });
-
+});
